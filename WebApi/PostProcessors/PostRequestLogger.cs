ef1c23937d411ca9432cba381aa1d2436f7a27d9
sewright22 <sewright22@gmail.com>
2022-03-19 16:21:48 -0500

    Added post logger

---
+++ WebApi/PostProcessors/PostRequestLogger.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using FastEndpoints.Validation;
+
+namespace WebApi.PostProcessors
+{
+    public class PostRequestLogger<TRequest, TResponse> : IPostProcessor<TRequest, TResponse>
+    {
+        public Task PostProcessAsync(TRequest req, TResponse res, HttpContext ctx, IReadOnlyCollection<ValidationFailure> failures, CancellationToken ct)
+        {
+            var logger = ctx.RequestServices.GetRequiredService<ILogger<TResponse>>();
+
+            logger.LogInformation($"request:{res?.GetType().FullName} path: {ctx.Request.Path}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
